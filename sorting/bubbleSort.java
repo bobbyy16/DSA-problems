ed75c2df6c2b0828204d042ab8300b31aa624e7b
Abhishek k <bobbyyyyy16@gmail.com>
2022-07-19 12:03:07 +0530

    added Sorting Algorithms

---
+++ sorting/bubbleSort.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2,56,0,9,87,1,3};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i ; j++) {
+                if(arr[j] < arr[j - 1]){  //arr[j] is 1 index and arr[j - 1] is 0th index at 1st interation
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
